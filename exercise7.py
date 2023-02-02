fc0243fb0a014e3f9ea3bce8b3153021c81868c5
Adrian Maziarz <adrian.maziarz.92@gmail.com>
2023-02-02 16:12:49 +0100

    Adding exercise7.py

---
+++ exercise7.py
@@ -0,0 +1,5 @@
+s = 'This is string where i want to find word "this", \
+and this is a problem which I need to solve in this moment\
+and another ptoblem is how to find "this" whit a uppercase, but it is easy.'
+print(s)
+print("it's", s.lower().count('this'), 'times :-D')
