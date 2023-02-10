ece1831a65988187d77a23fea7e56b801a4a191f
Adrian Maziarz <adrian.maziarz.92@gmail.com>
2023-02-10 21:13:55 +0100

    Tuple exercises

---
+++ Python Tuple Exercise with Solutions/exercise1.py
@@ -0,0 +1,7 @@
+tuple1 = (10, 20, 30, 40, 50)
+
+# 1
+s = slice(None, None, -1)
+print(tuple1[s])
+# or 2
+print(tuple1[::-1])
---
+++ Python Tuple Exercise with Solutions/exercise7.py
@@ -0,0 +1,4 @@
+tuple1 = (11, [22, 33], 44, 55)
+print(tuple1)
+tuple1[1][0] = 222
+print(tuple1)
