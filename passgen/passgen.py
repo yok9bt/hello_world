0f867675ee87479b8ca3705d52bac7e2793512db
Adrian Maziarz <adrian.maziarz.92@gmail.com>
2023-08-28 20:07:04 +0200

    work on binary data, not on hex string

--- passgen/passgen.py
+++ passgen/passgen.py
@@ -6,19 +6,19 @@
 import time
 import hashlib
 
-def do_iterations(data = 'Give me data!', number = 0, verbose = True):
+def do_iterations(data = 'Give me data!'.encode(), number = 0, verbose = True):
     for i in range(number):
         for _ in range(int(1e6)):
-            data = hashlib.sha3_512(data.encode()).hexdigest()
-        if verbose: print(data, ' (iteracja: ', i+1,') (pozostało: ', number-(i+1), ')', sep='')
+            data = hashlib.sha3_512(data).digest()
+        if verbose: print(data.hex(), ' (iteracja: ', i+1,') (pozostało: ', number-(i+1), ')', sep='')
 
 def speed_test(test_loops = 1):   # output is in MH/h
     start = time.time()
-    do_iterations('speed_test', test_loops, False)
+    do_iterations('speed_test'.encode(), test_loops, False)
     return int((test_loops / (time.time() - start))*3600)
 
 def main():
-    string1 = input('String wejściowy: ')
+    string1 = input('String wejściowy: ').encode()
     time1 = float(input('Czas generowania [h]: '))
     speed1 = speed_test()
     print('Wydajność:', str(speed1), 'iteracji/h'); time.sleep(1)
