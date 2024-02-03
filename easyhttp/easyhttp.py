a6e08f7584400bb795b25f7a3d68c8b5542f44dd
Adrian Maziarz <adrian.maziarz.92@gmail.com>
2024-02-03 14:31:59 +0100

    change in easyhttp

--- easyhttp/easyhttp.py
+++ easyhttp/easyhttp.py
@@ -1,19 +1,16 @@
-# Python 3 server example
+#!/bin/python3
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import secrets
 
-hostName = "localhost"
+hostName = "147.135.209.249"
 serverPort = 80
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.wfile.write(bytes("<html><head><title>MAZIARZ.TECH</title></head>", "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>MAZIARZ.TECH</p>", "utf-8"))
-        self.wfile.write(bytes("<p>losowy hex: ", "utf-8"))
-        self.wfile.write(bytes(secrets.token_hex(4), "utf-8"))
-        self.wfile.write(bytes("</p>", "utf-8"))
+        self.wfile.write(bytes(secrets.token_hex(8), "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
